a09ef06b8476534e70e6cba531ca6b78a9fab0b4
Matthew Cataldi <mcataloe@gmail.com>
2018-12-31 18:00:21 -0600

    Added Modal component for Delete confirm with Delete component.  Used portals to work-around issue of CSS and style with sibilings/children.  Modified index.html to have a div element to reference portal modal.

---
+++ client/src/components/Modal.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import history from '../history';
+
+const Modal = props => {
+    return ReactDOM.createPortal(
+        <div onClick={ () => history.push('/') } className="ui dimmer modals visible active">
+            <div onClick={ (event) => event.stopPropagation() } className="ui standard modal visible active">
+                <div className="header">{props.title || "INSERT TITLE"}</div>
+                <div className="content">{props.content}</div>
+                <div className="actions">{props.actions}</div>
+            </div>
+        </div>, document.querySelector('#modal')
+    );
+};
+
+export default Modal;
