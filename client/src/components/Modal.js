38b4356b1f0d1b3d11b17c3c26c416d7102fcf70
Matthew Cataldi <mcataloe@gmail.com>
2018-12-31 18:49:36 -0600

    Updated action file to redirect user after deleting a stream.  Updated StreamDelete and StreamList to incorporate related functionality.

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
     return ReactDOM.createPortal(
-        <div onClick={ () => history.push('/') } className="ui dimmer modals visible active">
+        <div onClick={ props.onDismiss } className="ui dimmer modals visible active">
             <div onClick={ (event) => event.stopPropagation() } className="ui standard modal visible active">
                 <div className="header">{props.title || "INSERT TITLE"}</div>
                 <div className="content">{props.content}</div>
